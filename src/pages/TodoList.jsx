aabdd0e6519ff32d4b393612b0583544ab325e2b
Sanojan Thiyagaraja <sanojan99@gmail.com>
2022-02-22 02:00:39 +1100

    finished todo list

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,43 +1,76 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  MixedCheckbox,
-  CustomCheckbox,
-  CustomCheckboxContainer,
-  CustomCheckboxInput,
-} from "@reach/checkbox";
-import "@reach/checkbox/styles.css";
+import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
+import { BsPlusLg } from "react-icons/bs";
+import { Input, UnstyledBtn } from "../components/styles/Styles";
 
 function TodoList() {
   const [todos, setTodos] = React.useState([
     "Check off this task",
     "Add a todo",
+    "Do stuff",
   ]);
 
-  const [checked, setChecked] = React.useState(true);
+  const [newTodo, setNewTodo] = React.useState("");
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (todo) => {
+    // delay removal todo from list to show strikethrough momentarily
+    // setTimeout(() => {
+    //   setTodos((todos) => todos.filter((item) => item !== todo));
+    // }, 1000);
+    setTodos((todos) => todos.filter((item) => item !== todo));
+  };
 
   return (
-    <TodoCont>
-      {todos.map((todo) => (
-        <Todo>
-          <label htmlFor="">
-            <CustomCheckbox
-              checked={true}
-              style={{
-                border: "2px solid white",
-              }}
-              onChange={() => setChecked(!checked)}
-            />
-          </label>
+    <>
+      <form onSubmit={addNewTodo}>
+        <AddTodoCont>
+          <Input
+            value={newTodo}
+            onChange={(e) => {
+              setNewTodo(e.target.value);
+            }}
+            placeholder="Add todo..."
+            style={{ width: "400px" }}
+          />
+          <UnstyledBtn>
+            <AddTodoIcon />
+          </UnstyledBtn>
+        </AddTodoCont>
+      </form>
 
-          {todo}
-        </Todo>
-      ))}
-    </TodoCont>
+      <TodoCont>
+        {todos.map((todo) => (
+          <Todo todo={todo} removeTodo={removeTodo} />
+        ))}
+      </TodoCont>
+    </>
   );
 }
 
 export default TodoList;
+
+function Todo({ todo, removeTodo }) {
+  const [isChecked, setIsChecked] = React.useState(false);
+  return (
+    <TodoWrapper>
+      <CheckboxWrapper
+        onClick={() => {
+          setIsChecked(!isChecked);
+          // if (!isChecked) removeTodo(todo);
+        }}
+      >
+        {isChecked ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />}
+      </CheckboxWrapper>
+      <TodoText isChecked={isChecked}>{todo}</TodoText>
+    </TodoWrapper>
+  );
+}
 
 const TodoCont = styled.div`
   display: flex;
@@ -45,7 +78,7 @@ const TodoCont = styled.div`
   gap: 10px;
 `;
 
-const Todo = styled.div`
+const TodoWrapper = styled.div`
   background-color: #282844;
   padding: 20px 30px;
   width: 400px;
@@ -53,4 +86,29 @@ const Todo = styled.div`
   display: flex;
   align-items: center;
   gap: 15px;
+`;
+
+const CheckboxWrapper = styled.div`
+  color: #44446a;
+  display: flex;
+  align-items: center;
+`;
+
+const TodoText = styled.div`
+  text-decoration: ${(props) => (props.isChecked ? "line-through" : "")};
+`;
+
+const AddTodoCont = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const AddTodoIcon = styled(BsPlusLg)`
+  color: #5773ff;
+  width: 2em;
+  height: 2em;
+  &:hover {
+    color: #3650d2;
+  }
 `;
