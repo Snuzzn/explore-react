3782bcb46799dff8760ca4c82d9f1736a0f36262
Sanojan <sanojan99@gmail.com>
2022-04-26 17:36:15 +1000

    finish main buttons in styled components page

--- src/pages/StyledComponents.jsx
+++ src/pages/StyledComponents.jsx
@@ -2,13 +2,41 @@ import React from "react";
 import styled from "styled-components";
 import DemoCont from "../components/DemoCont";
 
+const COLORS = {
+  blue: {
+    "--defaultColor": "#2f48c8",
+    "--textColor": "#5773ff",
+    "--hoverColor": "#2f48c85f",
+    "--focusColor": "#4760dc",
+  },
+  pink: {
+    "--defaultColor": "#DB2777",
+    "--textColor": "#c25586",
+    "--hoverColor": "#db277865",
+    "--focusColor": "#f676b4",
+  },
+};
+
 const StyledComponents = () => {
   return (
     <>
       <DemoCont>
-        <Button>Button</Button>
-        <OutlineBtn>Button</OutlineBtn>
-        <GhostBtn>Button</GhostBtn>
+        <ButtonsWrapper>
+          <Flex>
+            <Button>Button</Button>
+            <Button variant="outline">Button</Button>
+            <Button variant="ghost">Button</Button>
+          </Flex>
+          <Flex>
+            <Button color="pink">Button</Button>
+            <Button variant="outline" color="pink">
+              Button
+            </Button>
+            <Button variant="ghost" color="pink">
+              Button
+            </Button>
+          </Flex>
+        </ButtonsWrapper>
       </DemoCont>
     </>
   );
@@ -16,33 +44,56 @@ const StyledComponents = () => {
 
 export default StyledComponents;
 
-const Button = styled.button`
-  color: white;
+const Button = ({ variant, color, children }) => {
+  let styles = COLORS[color];
+  if (!styles) styles = COLORS["blue"];
+
+  let Component;
+  if (variant === "outline") Component = OutlineBtn;
+  else if (variant === "ghost") Component = GhostBtn;
+  else Component = BaseBtn;
+
+  return <Component style={styles}>{children}</Component>;
+};
+
+const BaseBtn = styled.button`
+  background-color: var(--defaultColor);
   padding: 10px 20px;
   border-radius: 10px;
   font-size: 14pt;
   border: 2px solid transparent;
-  background-color: #2f48c8;
   transition: 200ms background-color ease;
   cursor: pointer;
   &:hover {
-    background-color: #253791;
+    background-color: var(--hoverColor);
   }
   &:focus {
-    outline: 2px solid #4760dc;
+    outline: 2px solid var(--focusColor);
     outline-offset: 2px;
   }
 `;
 
-const OutlineBtn = styled(Button)`
+const OutlineBtn = styled(BaseBtn)`
+  color: var(--textColor);
+  border: 2px solid var(--defaultColor);
   background: none;
-  color: #5773ff;
-  border: 2px solid #2f48c8;
   &:hover {
-    background-color: #2f48c843;
+    background-color: var(--hoverColor);
+    /* opacity: 30%; */
   }
 `;
 
 const GhostBtn = styled(OutlineBtn)`
   border-color: transparent;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 25px;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
 `;
