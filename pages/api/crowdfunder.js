97cdc19efe620c2ac1f07ea9d7bb9edcbc0eabe0
Sanojan <sanojan99@gmail.com>
2022-05-04 17:08:37 +1000

    removed persistence

--- pages/api/crowdfunder.js
+++ pages/api/crowdfunder.js
@@ -1,26 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const fs = require("fs");
 
+const crowdfunderPost = {
+  title: "Genie's lamp",
+  content:
+    "An oil lamp that holds a genie. Once released, you will be granted 3 wishes!",
+  raisedSoFar: 100,
+  target: 1000,
+  img: "/images/genieLamp.jpg",
+};
+
 const handler = async (req, res) => {
-  const crowdfunder = JSON.parse(
-    fs.readFileSync("crowdfunder.json", "utf8", (err, jsonString) => {
-      if (err) {
-        console.log("File read failed:", err);
-        return;
-      }
-    })
-  );
+  // const crowdfunder = JSON.parse(
+  //   fs.readFileSync("crowdfunder.json", "utf8", (err, jsonString) => {
+  //     if (err) {
+  //       console.log("File read failed:", err);
+  //       return;
+  //     }
+  //   })
+  // );
 
   if (req.method === "GET") {
-    const currTimestamp = new Date();
-    // raisedSoFar += 1;
-    // fs.writeFileSync("crowdfunder.json", JSON.stringify(raisedSoFar));
-    res.status(200).json(crowdfunder);
+    // const currTimestamp = new Date();
+    res.status(200).json(crowdfunderPost);
   } else if (req.method === "POST") {
-    console.log(req.body);
-    crowdfunder.raisedSoFar += req.body.backingAmount;
-    fs.writeFileSync("crowdfunder.json", JSON.stringify(crowdfunder));
-    res.status(200).json(crowdfunder);
+    crowdfunderPost.raisedSoFar += req.body.backingAmount;
+    // fs.writeFileSync("crowdfunder.json", JSON.stringify(crowdfunder));
+    res.status(200).json(crowdfunderPost);
   }
 };
 
