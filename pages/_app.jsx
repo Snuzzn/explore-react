54a2b59ebbeda478977555ac8d6dece42b524baa
Sanojan <sanojan99@gmail.com>
2022-05-06 22:18:12 +1000

    fixing initial footer render bug

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import SiteLayout from "components/SiteLayout";
 import Head from "next/head";
 import { Suspense } from "react";
 import styled from "styled-components";
@@ -23,56 +24,12 @@ function MyApp({ Component, pageProps }) {
         <title>Explore React</title>
       </Head>
       <ThemeProvider>
-        <PageContainer>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-          <Footer>
-            <FooterContent>
-              Note: Code snippets do not include styling details to keep things
-              concise.
-            </FooterContent>
-            <FooterContent>Copyright © 2022 Explore React</FooterContent>
-          </Footer>
-        </PageContainer>
+        <SiteLayout>
+          <Component {...pageProps} />
+        </SiteLayout>
       </ThemeProvider>
     </>
   );
 }
 
 export default MyApp;
-
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
-
-const Layout = styled.div`
-  display: flex;
-  gap: 25px;
-  padding: 10px;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 15px;
-  flex-grow: 1;
-  /* height: 100%; */
-`;
-
-const Footer = styled.div`
-  margin-top: 70px;
-  border-top: 1px solid #313944;
-  display: flex;
-  align-items: end;
-  justify-content: center;
-  padding: 60px 30px;
-  font-size: 1.1rem;
-  color: #6b7380;
-  /* flex-direction: column; */
-  justify-content: space-between;
-`;
-
-const FooterContent = styled.p`
-  margin: 4px;
-  /* max-width: 450px; */
-`;
